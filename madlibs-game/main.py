77492f62c9fd95119876a58adeaf62077fb9d4c3
dyljaya07 <dylchirath21@gmail.com>
2026-02-02 14:39:49 +1100

    Added madlibs game

---
+++ madlibs-game/main.py
@@ -0,0 +1,16 @@
+# Exercise 3 Madlibs Game
+
+# User Input
+
+adjective1 = input("Please enter your adjective (description): ")
+noun1 = input("Please enter your noun (person, place, thing): ")
+adjective2 = input("Please enter an adjective (description): ")
+verb1 = input("Please enter a verb ending with 'ing': ")
+adjective3 = input("Please enter an adjective (description): ")
+
+# Story
+
+print(f"Today I went to a {adjective1} circus.")
+print(f"In an exhibit, I saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}!")
