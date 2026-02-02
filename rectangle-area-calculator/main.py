e5a3ff1a607c547bf07c844b38505ce830acf2f6
dyljaya07 <dylchirath21@gmail.com>
2026-02-02 13:53:19 +1100

    Added completed rectangle area calculator

---
+++ rectangle-area-calculator/main.py
@@ -0,0 +1,10 @@
+# Exercise 1 Rectangle Area Calculator
+
+# Variables
+length = input("Please enter the length of the rectangle: ")
+width = input("Please enter the width of the rectangle: ")
+area = float(length) * float(width)
+
+# Final Answer
+print(f"The area of the rectangle is {area}cm²")
+
