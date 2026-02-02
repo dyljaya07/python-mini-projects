7b8ba247bd533b0577117269611575e4e3395c3d
dyljaya07 <dylchirath21@gmail.com>
2026-02-02 17:17:08 +1100

    Added circumference of a circle

---
+++ circumference-of-a-circle/main.py
@@ -0,0 +1,7 @@
+import math
+
+radius = float(input("Please enter the radius of the circle: "))
+
+circumference = 2 * math.pi * radius
+
+print(f"The circumference of this circle is {round(circumference, 2)}cm")
