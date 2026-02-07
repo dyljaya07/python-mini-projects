66a006d45ce4753e93a706a7928f8b16a4cc2b40
dyljaya07 <dylchirath21@gmail.com>
2026-02-08 10:45:23 +1100

    Added basic-banking program

---
+++ basic-banking/README.md
@@ -0,0 +1,2 @@
+# Basic Banking
+Simple program to allow users to deposit and withdraw money.
---
+++ basic-banking/main.py
@@ -0,0 +1,53 @@
+def show_balance(balance):
+    print(f"Your balance is ${balance:.2f}")
+
+def deposit():
+    amount = float(input("Enter amount to deposit: "))
+
+    if amount < 0:
+        print("That's not a valid amount")
+        return 0
+    else:
+        return amount
+
+def withdraw(balance):
+    amount = float(input("Enter amount to withdraw: "))
+
+    if amount > balance:
+        print("Insufficient funds")
+        return 0
+    elif amount < 0:
+        print("Amount cannot be negative")
+        return 0
+    else:
+        print(f"You've withdrew ${amount:.2f}")
+        return amount
+
+def main():
+    balance = 0
+    is_running = True
+
+    while is_running:
+        print("Banking Program")
+        print("1. Show Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
+
+        choice = input("Enter your choice (1-4): ")
+
+        if choice == "1":
+            show_balance(balance)
+        elif choice == "2":
+            balance += deposit()
+        elif choice == "3":
+            balance -= withdraw(balance)
+        elif choice == "4":
+            is_running = False
+        else:
+            print("Invalid choice")
+
+    print("Thank you have a nice day!")
+
+if __name__ == "__main__":
+    main()
