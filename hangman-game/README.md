96fc53c2420f04fea6f306ac5df6cd5e473d0b98
dyljaya07 <dylchirath21@gmail.com>
2026-02-09 10:49:39 +1100

    Added hangman-game

---
+++ hangman-game/README.md
@@ -0,0 +1,2 @@
+# Hangman Game
+A small command-line game that allows users to play hang-man.
---
+++ hangman-game/wordslist.py
@@ -0,0 +1,37 @@
+# words for hangman game
+
+words = (
+    "apple", "orange", "banana", "grape", "pear", "peach", "plum", "cherry",
+    "strawberry", "blueberry", "raspberry", "blackberry", "melon", "watermelon",
+    "kiwi", "mango", "papaya", "pineapple", "coconut", "lemon", "lime",
+
+    "dog", "cat", "horse", "sheep", "cow", "goat", "pig", "rabbit", "mouse",
+    "tiger", "lion", "zebra", "giraffe", "elephant", "monkey", "panda",
+    "kangaroo", "koala", "bear", "wolf", "fox", "deer",
+
+    "house", "window", "door", "table", "chair", "couch", "bed", "pillow",
+    "blanket", "mirror", "clock", "lamp", "shelf", "drawer", "carpet",
+
+    "computer", "keyboard", "monitor", "mousepad", "printer", "laptop",
+    "internet", "software", "hardware", "network", "database", "program",
+    "python", "function", "variable", "loop", "array", "dictionary",
+
+    "school", "teacher", "student", "homework", "lesson", "classroom",
+    "notebook", "backpack", "pencil", "eraser", "library", "exam",
+
+    "river", "mountain", "forest", "desert", "ocean", "island", "valley",
+    "volcano", "waterfall", "glacier", "beach", "canyon",
+
+    "music", "guitar", "piano", "drums", "violin", "trumpet", "concert",
+    "playlist", "headphones", "microphone",
+
+    "sports", "soccer", "basketball", "baseball", "tennis", "cricket",
+    "swimming", "cycling", "running", "gymnastics",
+
+    "happy", "sad", "angry", "excited", "nervous", "confident", "curious",
+    "brave", "calm", "proud", "relaxed",
+
+    "adventure", "mystery", "fantasy", "science", "history", "culture",
+    "language", "freedom", "knowledge", "progress"
+)
+
